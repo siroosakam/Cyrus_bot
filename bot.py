a75fe27a67a8d02d7591b91dc9ac7674a94c6c09
siroosakam <siroossober@gmail.com>
2025-08-08 22:58:48 +0330

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,3 +1,18 @@
+from flask import Flask
+import threading
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = threading.Thread(target=run)
+t.start()
+
 import telebot
 from telebot import types
 
